a14d2ecf15f4470cdb55413462bc0396918dd6ba
saanandrimsha47 <epistemiaforensics47@gmail.com>
2026-08-24 20:58:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import os
 import gradio as gr
-from PIL import Image
 
 def predict(img):
-    return "Model loaded - Fake/Real prediction will show here"
+    return "Working! Model will be added next"
 
 port = int(os.environ.get("PORT", 10000))
-print(f"Starting on 0.0.0.0:{port}")
-
 demo = gr.Interface(fn=predict, inputs=gr.Image(type="pil"), outputs=gr.Textbox(), title="DeepGuard Lite")
 demo.launch(server_name="0.0.0.0", server_port=port)
