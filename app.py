c9e7cc84636fae10d99087014f2b375e021e1de2
saanandrimsha47 <epistemiaforensics47@gmail.com>
2026-09-05 15:48:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,4 +57,5 @@ with gr.Blocks() as demo:
             vid_out = gr.Textbox(label="Result")
             gr.Button("Detect Video").click(predict_video, vid_in, vid_out)
 
-demo.launch()
+import os
+demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 10000)))
